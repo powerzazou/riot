28109151a13958a28f21766b449a392511068928
superzazou <vincent.e.dumont@gmail.com>
2016-01-23 17:43:18 +0100

    add sails server

---
+++ server/api/controllers/PlayerController.js
@@ -0,0 +1,21 @@
+/**
+ * PlayerController
+ *
+ * @description :: Server-side logic for managing players
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+
+
+  /**
+   * `PlayerController.get()`
+   */
+  get: function (req, res) {
+    return res.json({
+      todo: 'get() is not implemented yet!'
+    });
+  }
+};
+
